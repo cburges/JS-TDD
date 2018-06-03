33e4094b8e66014769f2547866d92501c8430604
Colin Burgess <burgess.colin3@gmail.com>
2018-06-03 09:00:28 -0400

    First test implementation

---
+++ flickr-fetcher-spec.js
@@ -0,0 +1,44 @@
+'use strict';
+var expect = require('chai').expect;
+var FlickrFetcher = require('./flickr-fetcher.js');
+
+describe ('FlickrFetcher', function() {
+  it('should exist', function() {
+    var FlickrFetcher = require('./flickr-fetcher.js');
+    expect(FlickrFetcher).to.not.be.undefined;
+  });
+});
+
+describe('#photoObjToUrl()', function() {
+  it('should take a photo object from Flickr and return a string', function() {
+    var input = {
+      id:       '24770505034',
+      owner:    '97248275@NO3',
+      secret:   '31a9986429',
+      server:   '1577',
+      farm:     2,
+      title:    '20160229090898',
+      ispublic: 1,
+      isfriend: 0,
+      isfamily: 0
+    };
+    var expected = 'https://farm2.staticflickr.com/1577/24770505034_31a9986429_b.jpg';
+    var actual = FlickrFetcher.photoObjToUrl(input);
+    expect(actual).to.eql(expected);
+
+    input = {
+      id:       '24770504484',
+      owner:    '97248275@N03',
+      secret:   '69dd90d5dd',
+      server:   '1451',
+      farm:     2,
+      title:    '20160229090903',
+      ispublic: 1,
+      isfriend: 0,
+      isfamily: 0
+    };
+    expected = 'https://farm2.staticflickr.com/1451/24770504484_69dd90d5dd_b.jpg';
+    actual = FlickrFetcher.photoObjToUrl(input);
+    expect(actual).to.eql(expected);
+  });
+});
